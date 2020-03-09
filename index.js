de22dd7c8baeab9e60a6247e556e290aadbeba91
Green Vine <barryaervin@gmail.com>
2020-03-09 10:07:06 -0400

    hey

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
-
+const prosemirror = require('./proseMirror/index')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-mongoose.connect("mongodb://localhost/dashboard", { useNewUrlParser: true })
+// mongoose.connect("mongodb://localhost/dashboard", { useNewUrlParser: true })
+
+app.get('/', function (req, res) {
+    prosemirror.exampleSetup(Object)
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
