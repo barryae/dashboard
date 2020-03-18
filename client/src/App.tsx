9de5d812c11698fa19037e3f73cb22425f5c56de
Green Vine <barryaervin@gmail.com>
2020-03-18 09:59:08 -0400

    added main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import Main from "./pages/Main"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>Hey!</p>
+      <Main />
     </div>
   );
 }
