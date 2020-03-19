7ec92722181abaf2fbe8c6fcd86ddec110a760b7
Green Vine <barryaervin@gmail.com>
2020-03-19 15:05:04 -0400

    got first chart working

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { data } from "./data"
 import Card from "../../components/Card"
 
 export default function Main() {
+
     return (
         <div>
             <h1>Main</h1>
-            <Card type={"type"} data={["Hey", "You"]} />
+            <Card type={"type"} data={data} />
         </div>
     )
+
 }
+
