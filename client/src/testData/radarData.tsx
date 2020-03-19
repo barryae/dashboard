be7927870211512b2577402e2319d3109f774855
Green Vine <barryaervin@gmail.com>
2020-03-19 15:24:58 -0400

    adding test data

---
+++ client/src/testData/radarData.tsx
@@ -0,0 +1,33 @@
+export const data =
+    [
+        {
+            "taste": "fruity",
+            "chardonay": 62,
+            "carmenere": 102,
+            "syrah": 81
+        },
+        {
+            "taste": "bitter",
+            "chardonay": 79,
+            "carmenere": 86,
+            "syrah": 52
+        },
+        {
+            "taste": "heavy",
+            "chardonay": 65,
+            "carmenere": 45,
+            "syrah": 86
+        },
+        {
+            "taste": "strong",
+            "chardonay": 73,
+            "carmenere": 118,
+            "syrah": 60
+        },
+        {
+            "taste": "sunny",
+            "chardonay": 50,
+            "carmenere": 86,
+            "syrah": 58
+        }
+    ]
