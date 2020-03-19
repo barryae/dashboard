1489f3e6702afd5bfc51a16dca02bc0a451f26b2
Green Vine <barryaervin@gmail.com>
2020-03-19 15:19:46 -0400

    init radar

--- client/src/components/Card/index.tsx
+++ client/src/components/Card/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { ResponsiveCalendar } from "nivo"
+import { ResponsiveCalendar, ResponsiveRadar } from "nivo"
 
 export default function Card(props: { type: string; data: { "day": string; "value": number; }[] }) {
     function createCard(type: string, data: { "day": string; "value": number; }[]) {
@@ -30,7 +30,56 @@ export default function Card(props: { type: string; data: { "day": string; "valu
                         }
                     ]}
                 />
-                break;
+            case 'ResponsiveRadar':
+                return <ResponsiveRadar
+                    data={data}
+                    keys={['chardonay', 'carmenere', 'syrah']}
+                    indexBy="taste"
+                    maxValue="auto"
+                    margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
+                    curve="linearClosed"
+                    borderWidth={2}
+                    borderColor={{ from: 'color' }}
+                    gridLevels={5}
+                    gridShape="circular"
+                    gridLabelOffset={36}
+                    enableDots={true}
+                    dotSize={10}
+                    dotColor={{ theme: 'background' }}
+                    dotBorderWidth={2}
+                    dotBorderColor={{ from: 'color' }}
+                    enableDotLabel={true}
+                    dotLabel="value"
+                    dotLabelYOffset={-12}
+                    colors={{ scheme: 'nivo' }}
+                    fillOpacity={0.25}
+                    blendMode="multiply"
+                    animate={true}
+                    motionStiffness={90}
+                    motionDamping={15}
+                    isInteractive={true}
+                    legends={[
+                        {
+                            anchor: 'top-left',
+                            direction: 'column',
+                            translateX: -50,
+                            translateY: -40,
+                            itemWidth: 80,
+                            itemHeight: 20,
+                            itemTextColor: '#999',
+                            symbolSize: 12,
+                            symbolShape: 'circle',
+                            effects: [
+                                {
+                                    on: 'hover',
+                                    style: {
+                                        itemTextColor: '#000'
+                                    }
+                                }
+                            ]
+                        }
+                    ]}
+                />
         }
     }
 
