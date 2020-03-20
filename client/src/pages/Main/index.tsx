1a32144a396bbefa01d84b2f75048b103ad7cefc
Green Vine <barryaervin@gmail.com>
2020-03-20 15:16:25 -0400

    tweaks

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { calendarData } from "../../testData/calendarData"
 import { radarData } from "../../testData/radarData"
+import { barData } from "../../testData/barData"
 import Card from "../../components/Card"
 
 export default function Main() {
@@ -10,6 +11,7 @@ export default function Main() {
             <h1>Operational Dashboard</h1>
             <Card type={"ResponsiveCalendar"} data={calendarData} />
             <Card type={"ResponsiveRadar"} data={radarData} />
+            <Card type={"ResponsiveBar"} data={barData} />
         </div>
     )
 
