63f9a03a5fdcd60a3444bf3b3dccd294c6094733
Green Vine <barryaervin@gmail.com>
2020-03-26 10:26:16 -0400

    tweaks

--- client/src/components/Card/index.tsx
+++ client/src/components/Card/index.tsx
@@ -186,7 +186,7 @@ export default function Card(props: { order: number; type: string; data: any[] }
 
                     <ResponsiveLine
                         data={data}
-                        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+                        margin={{ top: 50, right: 20, bottom: 50, left: 60 }}
                         xScale={{ type: 'point' }}
                         yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
                         axisTop={null}
