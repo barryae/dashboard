4dae74ab6e90b2b248915718f0e3b6ed40fe1073
Green Vine <barryaervin@gmail.com>
2020-03-31 11:02:45 -0400

    Updates

---
+++ precache-manifest.a0b8d0e3c4c8b1a97fb4baff497d71d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c7eb55876eefb39fd275e17d7afdbde",
+    "url": "/barryae/dashboard/index.html"
+  },
+  {
+    "revision": "37e840202f73fb97489c",
+    "url": "/barryae/dashboard/static/css/main.24265622.chunk.css"
+  },
+  {
+    "revision": "3143a1fe5558405ef104",
+    "url": "/barryae/dashboard/static/js/2.38174b42.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/barryae/dashboard/static/js/2.38174b42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37e840202f73fb97489c",
+    "url": "/barryae/dashboard/static/js/main.6c87a1c0.chunk.js"
+  },
+  {
+    "revision": "37294d6c5dafc5adc2fe",
+    "url": "/barryae/dashboard/static/js/runtime-main.b783353a.js"
+  }
+]);
